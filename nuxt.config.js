fcf7191498707b5e2114185ec1721c5af5cb0d44
diegoreginop <diego@finaktiva.com>
2021-09-21 22:57:01 -0500

    config jest, tailwind and axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,20 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global App headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'My Nuxt Application',
+    title: "My Nuxt Application",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'This is an awesome description of my Nuxt app',
+        hid: "description",
+        name: "description",
+        content: "This is an awesome description of my Nuxt app",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -23,8 +23,8 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ["@nuxtjs/tailwindcss"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: []
+  modules: ["@nuxtjs/axios"],
 };
